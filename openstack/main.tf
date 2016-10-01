0ccb4b511c44701f91abf8cf9b9983d9238e94d0
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-10-01 11:33:28 +0200

    add openstack

---
+++ openstack/main.tf
@@ -0,0 +1,57 @@
+variable cluster_prefix {}
+variable KuberNow_image {}
+variable keypair_name {}
+
+variable private_network {}
+variable floating_ip_pool {}
+
+variable master_count {}
+variable master_flavor {}
+variable master_volume_size {}
+
+variable node_count {}
+variable node_flavor {}
+variable node_volume_size {}
+
+variable etcd_count {}
+variable etcd_flavor {}
+variable etcd_volume_size {}
+
+module "kube-master" {
+  source = "./node"
+  name_prefix = "${var.cluster_prefix}"
+  entity_name = "master"
+  image_name = "${var.KuberNow_image}"
+  flavor_name = "${var.master_flavor}"
+  keypair_name = "${var.keypair_name}"
+  network_name = "${var.private_network}"
+  floating_ip_pool = "${var.floating_ip_pool}"
+  volume_size = "${var.master_volume_size}"
+  count = "${var.master_count}"
+}
+
+module "kube-node" {
+  source = "./node"
+  name_prefix = "${var.cluster_prefix}"
+  entity_name = "node"
+  image_name = "${var.KuberNow_image}"
+  flavor_name = "${var.node_flavor}"
+  keypair_name = "${var.keypair_name}"
+  network_name = "${var.private_network}"
+  floating_ip_pool = "${var.floating_ip_pool}"
+  volume_size = "${var.node_volume_size}"
+  count = "${var.node_count}"
+}
+
+module "etcd" {
+  source = "./node"
+  name_prefix = "${var.cluster_prefix}"
+  entity_name = "etcd"
+  image_name = "${var.KuberNow_image}"
+  flavor_name = "${var.etcd_flavor}"
+  keypair_name = "${var.keypair_name}"
+  network_name = "${var.private_network}"
+  floating_ip_pool = "${var.floating_ip_pool}"
+  volume_size = "${var.etcd_volume_size}"
+  count = "${var.etcd_count}"
+}
---
+++ openstack/node/main.tf
@@ -0,0 +1,40 @@
+variable name_prefix {}
+variable entity_name {}
+variable image_name {}
+variable flavor_name {}
+variable keypair_name {}
+variable network_name {}
+variable floating_ip_pool {}
+variable volume_size {}
+variable volume_device { default = "/dev/vdb" }
+variable count {}
+
+# Create volumes
+resource "openstack_blockstorage_volume_v1" "blockstorage" {
+  name = "${var.name_prefix}-${var.entity_name}-volume-${format("%03d", count.index)}"
+  size = "${var.volume_size}"
+  count = "${var.count}"
+}
+
+# Allocate floating IPs
+resource "openstack_compute_floatingip_v2" "floating_ip" {
+  pool = "${var.floating_ip_pool}"
+  count = "${var.count}"
+}
+
+# Create instances
+resource "openstack_compute_instance_v2" "instance" {
+  name="${var.name_prefix}-${var.entity_name}-${format("%03d", count.index)}"
+  image_name = "${var.image_name}"
+  flavor_name = "${var.flavor_name}"
+  floating_ip = "${element(openstack_compute_floatingip_v2.floating_ip.*.address, count.index)}"
+  key_pair = "${var.keypair_name}"
+  volume = {
+    volume_id = "${element(openstack_blockstorage_volume_v1.blockstorage.*.id, count.index)}"
+    device = "${var.volume_device}"
+  }
+  network {
+    name = "${var.network_name}"
+  }
+  count = "${var.count}"
+}
