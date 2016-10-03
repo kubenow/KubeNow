51d67626758f45b1925ddeab443ffed8c248d012
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-10-03 10:57:20 +0200

    clean up

--- openstack/main.tf
+++ openstack/main.tf
@@ -5,17 +5,10 @@ variable keypair_name {}
 variable private_network {}
 variable floating_ip_pool {}
 
-variable master_count {}
 variable master_flavor {}
-variable master_volume_size {}
 
-variable node_count {}
-variable node_flavor {}
-variable node_volume_size {}
-
-variable etcd_count {}
-variable etcd_flavor {}
-variable etcd_volume_size {}
+variable worker_count {}
+variable worker_flavor {}
 
 module "master" {
   source = "./node"
@@ -26,39 +19,17 @@ module "master" {
   keypair_name = "${var.keypair_name}"
   network_name = "${var.private_network}"
   floating_ip_pool = "${var.floating_ip_pool}"
-  volume_size = "${var.master_volume_size}"
-  count = "${var.master_count}"
+  count = "1"
 }
 
-module "node" {
+module "worker" {
   source = "./node"
   name_prefix = "${var.cluster_prefix}"
-  entity_name = "node"
+  entity_name = "worker"
   image_name = "${var.KuberNow_image}"
-  flavor_name = "${var.node_flavor}"
+  flavor_name = "${var.worker_flavor}"
   keypair_name = "${var.keypair_name}"
   network_name = "${var.private_network}"
   floating_ip_pool = "${var.floating_ip_pool}"
-  volume_size = "${var.node_volume_size}"
-  count = "${var.node_count}"
-}
-
-module "etcd" {
-  source = "./node"
-  name_prefix = "${var.cluster_prefix}"
-  entity_name = "etcd"
-  image_name = "${var.KuberNow_image}"
-  flavor_name = "${var.etcd_flavor}"
-  keypair_name = "${var.keypair_name}"
-  network_name = "${var.private_network}"
-  floating_ip_pool = "${var.floating_ip_pool}"
-  volume_size = "${var.etcd_volume_size}"
-  count = "${var.etcd_count}"
-}
-
-module "inventory_gen" {
-  source = "./inventory"
-  master_inventory = "${module.master.inventory}"
-  node_inventory = "${module.node.inventory}"
-  etcd_inventory = "${module.etcd.inventory}"
+  count = "${var.worker_count}"
 }
--- openstack/node/main.tf
+++ openstack/node/main.tf
@@ -5,16 +5,7 @@ variable flavor_name {}
 variable keypair_name {}
 variable network_name {}
 variable floating_ip_pool {}
-variable volume_size {}
-variable volume_device { default = "/dev/vdb" }
 variable count {}
-
-# Create volumes
-resource "openstack_blockstorage_volume_v1" "blockstorage" {
-  name = "${var.name_prefix}-${var.entity_name}-volume-${format("%03d", count.index)}"
-  size = "${var.volume_size}"
-  count = "${var.count}"
-}
 
 # Allocate floating IPs
 resource "openstack_compute_floatingip_v2" "floating_ip" {
@@ -29,16 +20,8 @@ resource "openstack_compute_instance_v2" "instance" {
   flavor_name = "${var.flavor_name}"
   floating_ip = "${element(openstack_compute_floatingip_v2.floating_ip.*.address, count.index)}"
   key_pair = "${var.keypair_name}"
-  volume = {
-    volume_id = "${element(openstack_blockstorage_volume_v1.blockstorage.*.id, count.index)}"
-    device = "${var.volume_device}"
-  }
   network {
     name = "${var.network_name}"
   }
   count = "${var.count}"
-}
-
-output "inventory" {
-  value = "${join("\n",formatlist("%s ansible_ssh_host=%s", openstack_compute_instance_v2.instance.*.name, openstack_compute_floatingip_v2.floating_ip.*.address))}"
 }
