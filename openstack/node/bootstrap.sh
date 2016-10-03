1fe82f81502ff92545cf2b82ddf15b70217cb773
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-10-03 17:09:29 +0200

    avoid sleep reminder

--- openstack/node/bootstrap.sh
+++ openstack/node/bootstrap.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "Sleep 60 seconds..." #Give the master some time to start
-sleep 60
+sleep 60 # We need something smarter here
 
 echo "Joining the master..."
 kubeadm join --token ${kubeadm_token} ${master_ip}
