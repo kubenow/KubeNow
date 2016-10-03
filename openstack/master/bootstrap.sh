4b9ab2f5a3ba95f993b79415434d307658c06b5c
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-10-03 16:41:48 +0200

    add weave network

--- openstack/master/bootstrap.sh
+++ openstack/master/bootstrap.sh
@@ -5,3 +5,9 @@ set -e
 
 echo "Inititializing the master..."
 kubeadm init --token ${kubeadm_token}
+
+echo "Waiting for nodes..."
+sleep 120 # We need something smarter here
+
+echo "Deploying wave"
+kubectl apply -f https://git.io/weave-kube
