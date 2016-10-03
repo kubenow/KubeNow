e3eae31189c9e1e84f3062cd91dfc0ae9cda67a3
Anders Larsson <anders.larsson@icm.uu.se>
2016-10-03 22:44:22 +0200

    added wait loop until master listens before trying to joinin the node

--- openstack/node/bootstrap.sh
+++ openstack/node/bootstrap.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
-set -e
+# set -e
 
-echo "Sleep 60 seconds..." #Give the master some time to start
-sleep 60 # We need something smarter here
+sleeptime=5
+max_try_connect_time=60
+cumulative_wait_time=0
+echo "Try to join master..."
+while :; do
 
-echo "Joining the master..."
-kubeadm join --token ${kubeadm_token} ${master_ip}
+  # Try to join the master
+  kubeadm join --token ${kubeadm_token} ${master_ip} # 2>/dev/null
+  # if join exit code was OK then exit loop
+  if [ $? -eq 0 ]; then
+    echo "Joinined the master..."
+    exit 0
+  fi
+  
+  if [ $cumulative_wait_time -gt $max_try_connect_time ]; then
+    echo "Could not join to master in $max_try_connect_time seconds" >&2
+    exit 1
+  fi
+  
+  echo "Sleep $sleeptime before trying to join master again..."
+  sleep $sleeptime
+  (( cumulative_wait_time += sleeptime ))
+  
+done
+
+
