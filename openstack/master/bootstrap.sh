8a7f0703240cec10376c9935556f9cd7ecdb064a
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-10-04 16:54:00 +0200

    add-on refactory

--- openstack/master/bootstrap.sh
+++ openstack/master/bootstrap.sh
@@ -5,13 +5,3 @@ set -e
 
 echo "Inititializing the master..."
 kubeadm init --token ${kubeadm_token}
-
-echo "Waiting for nodes..."
-while [ $(kubectl get nodes | grep Ready | wc -l) \
-  -lt $((${node_count} + 1)) ]; do
-  sleep 20
-  echo "... waiting for nodes ..."
-done
-
-echo "Deploying wave"
-kubectl apply -f https://git.io/weave-kube
