2d0bc7dfe97a092f9e360fb8116e63ecd10d3ec4
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-11-07 19:18:48 +0100

    make docker installation more robust

--- packer/requirements.sh
+++ packer/requirements.sh
@@ -3,9 +3,21 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+echo "Ensure that APT works with HTTPS..."
+sudo apt-get update
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates
+
 echo "Add Kubernetes repo..."
 sudo sh -c 'curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -'
 sudo sh -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
+
+echo "Add Docker repo..."
+sudo apt-key adv \
+  --keyserver hkp://p80.pool.sks-keyservers.net:80 \
+  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list'
 
 echo "Updating Ubuntu..."
 sudo apt-get update -y
@@ -13,7 +25,9 @@ sudo apt-get upgrade -y
 
 echo "Installing Kubernetes requirements..."
 sudo apt-get install -y \
-  docker.io \
+  linux-image-extra-$(uname -r) \
+  linux-image-extra-virtual \
+  docker-engine \
   kubelet \
   kubeadm \
   kubectl \
