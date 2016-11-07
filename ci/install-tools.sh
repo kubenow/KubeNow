353ba7ac9e1d779f92d1dfaf429f0e126fd1f0d9
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-11-07 13:44:41 +0100

    remove sudo

--- ci/install-tools.sh
+++ ci/install-tools.sh
@@ -16,15 +16,15 @@ curl \
 curl \
   https://releases.hashicorp.com/terraform/0.7.8/terraform_0.7.8_linux_amd64.zip \
   > /tmp/terraform.zip
-sudo unzip /tmp/packer.zip -d /usr/bin
-sudo unzip /tmp/terraform.zip -d /usr/bin
-sudo chmod +x /usr/bin/packer
-sudo chmod +x /usr/bin/terraform
+unzip /tmp/packer.zip -d /usr/bin
+unzip /tmp/terraform.zip -d /usr/bin
+chmod +x /usr/bin/packer
+chmod +x /usr/bin/terraform
 
 # Add Ansible
 curl \
   https://bootstrap.pypa.io/get-pip.py \
   > /tmp/get-pip.py
-sudo python /tmp/get-pip.py
-sudo pip install --upgrade pip
-sudo pip install ansible==2.2.0.0
+python /tmp/get-pip.py
+pip install --upgrade pip
+pip install ansible==2.2.0.0
