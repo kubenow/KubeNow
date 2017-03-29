9371359cc0d47a8f4c088a4f727ebea397e262a1
Anders Larsson <anders.larsson@icm.uu.se>
2017-03-29 15:53:57 +0200

    fix kubernetes version in kubeadm [skip ci]

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -4,7 +4,7 @@ echo "Inititializing the master..."
 
 if [ -n "$api_advertise_addresses" ]
 then
-    kubeadm init --token ${kubeadm_token} --api-advertise-addresses=$api_advertise_addresses
+    kubeadm init --token ${kubeadm_token} --use-kubernetes-version=v1.5.2 --api-advertise-addresses=$api_advertise_addresses
 else
-    kubeadm init --token ${kubeadm_token}
+    kubeadm init --token ${kubeadm_token} --use-kubernetes-version=v1.5.2
 fi
