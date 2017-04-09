3c437aa42c869cbcffa2e74b2ad51975ee185336
Anders Larsson <anders.larsson@icm.uu.se>
2017-04-09 18:20:12 +0200

    added node labeling and removed wait cycle for kubeadm join (it is implemented in kubeadm)

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Label nodes"
+sed -i 's|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=${node_labels} |g' \
+       /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# reload and restart after systemd dropin edits
+systemctl daemon-reload
+systemctl restart kubelet
+
 echo "Inititializing the master..."
 
 if [ -n "$api_advertise_addresses" ]
--- bootstrap/node.sh
+++ bootstrap/node.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-SLEEPTIME=5 # Sec
-MAX_TRY_CONNECT_TIME=600 # Sec
 
-cumulative_wait_time=0
+# Add labels to node key=value pairs separated by ','
+echo "Label nodes"
+sed -i 's|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=${node_labels} |g' \
+       /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# reload and restart after systemd dropin edits
+systemctl daemon-reload
+systemctl restart kubelet
+
 echo "Try to join master..."
-while [ $cumulative_wait_time -lt $MAX_TRY_CONNECT_TIME ] ; do
+kubeadm join --token ${kubeadm_token} ${master_ip}
 
-  # Try to join the master
-  kubeadm join --token ${kubeadm_token} ${master_ip}
-  # If join exit code was OK then exit loop
-  if [ $? -eq 0 ]; then
-    echo "Joinined the master..."
-    exit 0
-  fi
-
-  echo "Sleep $SLEEPTIME before trying to join master again..."
-  sleep $SLEEPTIME
-  (( cumulative_wait_time += SLEEPTIME ))
-
-done
-
-echo "Could not join to master in $MAX_TRY_CONNECT_TIME seconds" >&2
-exit 1
