d2cf4923a98ceaaf4bdff7b9228af7c94759ca5e
Anders Larsson <anders.larsson@icm.uu.se>
2017-04-13 14:23:12 +0200

    revert to kubernetes 1.5.2

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -2,9 +2,16 @@
 
 # Add labels to node key=value pairs separated by ','
 echo "Label nodes"
-echo "Label and taint nodes"
 sed -i 's|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=${node_labels} |g' \
        /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+   
+echo "Taint nodes"    
+if [ -n "$node_taints" ]
+then
+    sed -i 's|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--register-with-taints=${node_taints} |g' \
+       /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+fi
+
 # reload and restart after systemd dropin edits
 systemctl daemon-reload
 systemctl restart kubelet
@@ -13,7 +20,7 @@ echo "Inititializing the master..."
 
 if [ -n "$api_advertise_addresses" ]
 then
-    kubeadm init --token ${kubeadm_token} --kubernetes-version=v1.5.2 --api-advertise-addresses=$api_advertise_addresses
+    kubeadm init --token ${kubeadm_token} --use-kubernetes-version=v1.5.2 --api-advertise-addresses=$api_advertise_addresses
 else
-    kubeadm init --token ${kubeadm_token} --kubernetes-version=v1.5.2
+    kubeadm init --token ${kubeadm_token} --use-kubernetes-version=v1.5.2
 fi
