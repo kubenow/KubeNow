940a345b365640ae2cb412cccb22f9bb466230bd
Anders Larsson <anders.larsson@icm.uu.se>
2017-04-22 08:20:40 +0200

    reverted port number in join ip (to comply with kubernetes 1.5.2)

--- bootstrap/node.sh
+++ bootstrap/node.sh
@@ -19,5 +19,5 @@ systemctl daemon-reload
 systemctl restart kubelet
 
 echo "Try to join master..."
-kubeadm join --token ${kubeadm_token} "${master_ip}:6443"
+kubeadm join --token ${kubeadm_token} "${master_ip}"
 
