17cc802bbced7b565a6ddc41dfc634badb269a03
Anders Larsson <anders.larsson@icm.uu.se>
2017-04-22 13:06:49 +0200

    fixed master and node template. only taint and label if var not empty

--- bootstrap/node.sh
+++ bootstrap/node.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+node_labels=${node_labels}
+node_taints=${node_taints}
+
 echo "Label nodes"
 if [ -n "$node_labels" ]
 then
-    sed -i 's|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=${node_labels} |g' \
+    sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=$node_labels |g" \
        /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 fi
    
 echo "Taint nodes"    
 if [ -n "$node_taints" ]
 then
-    sed -i 's|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--register-with-taints=${node_taints} |g' \
+    sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--register-with-taints=$node_taints |g" \
        /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 fi
 
