576f5d2aa748bb017322b19f4dc7310951c2a023
Anders Larsson <anders.larsson@icm.uu.se>
2017-05-15 22:09:11 +0200

    moved inventory generation to shared submodule

--- gce/main.tf
+++ gce/main.tf
@@ -134,50 +134,16 @@ module "cloudflare" {
 }
 
 # Generate Ansible inventory (identical for each cloud provider)
-resource "null_resource" "generate-inventory" {
-
-  # Trigger rewrite of inventory, uuid() generates a random string everytime it is called
-  triggers {
-    uuid = "${uuid()}"
-  }
-
-  # Write master
-  provisioner "local-exec" {
-    command =  "echo \"[master]\" > inventory"
-  }
-  # output the lists formated
-  provisioner "local-exec" {
-    command =  "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=ubuntu", module.master.hostnames, module.master.public_ip))}\" >> inventory"
-  }
-
-  # Write edges
-  provisioner "local-exec" {
-    command =  "echo \"[edge]\" >> inventory"
-  }
-  # output the lists formated
-  provisioner "local-exec" {
-    command =  "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=ubuntu", module.edge.hostnames, module.edge.public_ip))}\" >> inventory"
-  }
-  # only output if master is edge
-  provisioner "local-exec" {
-    command =  "echo \"${var.use_master_as_edge != true ? "" : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=ubuntu", module.master.hostnames, module.master.public_ip))}\" >> inventory"
-  }
-
-  # Write other variables
-  provisioner "local-exec" {
-    command =  "echo \"[master:vars]\" >> inventory"
-  }
-  provisioner "local-exec" {
-    command =  "echo \"nodes_count=${1 + var.edge_count + var.node_count} \" >> inventory"
-  }
-  provisioner "local-exec" {
-    command =  "echo \"node_count=${var.node_count} \" >> inventory"
-  }
-  # If cloudflare domain is set, output that domain, otherwise output a nip.io domain (with the first edge ip)
-  provisioner "local-exec" {
-    command =  "echo \"domain=${ var.use_cloudflare == true ? format("%s.%s", var.cluster_prefix, var.cloudflare_domain) : format("%s.nip.io", element(concat(module.edge.public_ip, module.master.public_ip), 0))}\" >> inventory"
-  }
-  provisioner "local-exec" {
-    command =  "echo \"use_cloudflare=${var.use_cloudflare}\" >> inventory"
-  }
+module "generate-inventory" {
+  source = "../common/inventory"
+  master_hostnames = "${module.master.hostnames}"
+  master_public_ip = "${module.master.public_ip}"
+  edge_hostnames = "${module.edge.hostnames}"
+  edge_public_ip = "${module.edge.public_ip}"
+  use_master_as_edge = "${var.use_master_as_edge}"
+  edge_count = "${var.edge_count}"
+  node_count = "${var.node_count}"
+  use_cloudflare = "${var.use_cloudflare}"
+  cluster_prefix = "${var.cluster_prefix}"
+  cloudflare_domain = "${var.cloudflare_domain}"
 }
