d454ef33dcb99a42159b22e2b924bf63aa1554d5
Anders Larsson <anders.larsson@icm.uu.se>
2017-06-06 18:30:55 +0200

    terraform output can not be empty string, therefore output space when no edges

--- common/inventory/main.tf
+++ common/inventory/main.tf
@@ -51,8 +51,9 @@ resource "null_resource" "generate-inventory" {
   }
 
   # output the lists formated, slice list to make sure hostname and ip-list have same length
+  # provisioner output can not be empty string - therefore output space when edge_count == 0
   provisioner "local-exec" {
-    command = "echo \"${var.edge_count == 0 ? "" : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=ubuntu", slice(var.edge_hostnames,0,var.edge_count), var.edge_public_ip))}\" >> inventory"
+    command = "echo \"${var.edge_count == 0 ? " " : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=ubuntu", slice(var.edge_hostnames,0,var.edge_count), var.edge_public_ip))}\" >> inventory"
   }
 
   # Write other variables
