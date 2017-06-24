3a0f3f0e56a469fc986d278e44c7ca3c62ccd8ca
Anders Larsson <anders.larsson@icm.uu.se>
2017-06-24 22:19:42 +0200

    add indentation to scripts to work in local-kvm

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -1,50 +1,53 @@
-#!/bin/bash
+       #!/bin/bash
 
-# Add hostname
-echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+       # OBS This file has to be indented 6 spaces to fit into cloud config yaml
+       # as inline bash file
 
-# Taint and label
-node_labels=${node_labels}
-node_taints=${node_taints}
+       # Add hostname
+       echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 
-echo "Label nodes"
-if [ -n "$node_labels" ]
-then
-    sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=$node_labels |g" \
-       /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-fi
+       # Taint and label
+       node_labels=${node_labels}
+       node_taints=${node_taints}
 
-echo "Taint nodes"
-if [ -n "$node_taints" ]
-then
-    sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--register-with-taints=$node_taints |g" \
-       /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-fi
+       echo "Label nodes"
+       if [ -n "$node_labels" ]
+       then
+           sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=$node_labels |g" \
+              /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+       fi
 
-# reload and restart after systemd dropin edits
-systemctl daemon-reload
-systemctl restart kubelet
+       echo "Taint nodes"
+       if [ -n "$node_taints" ]
+       then
+           sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--register-with-taints=$node_taints |g" \
+              /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+       fi
 
-# execute modprobe on node - workaround for heketi gluster
-echo "Modprobe dm_thin_pool..."
-modprobe dm_thin_pool
+       # reload and restart after systemd dropin edits
+       systemctl daemon-reload
+       systemctl restart kubelet
 
-echo "Inititializing the master...."
+       # execute modprobe on node - workaround for heketi gluster
+       echo "Modprobe dm_thin_pool..."
+       modprobe dm_thin_pool
 
-if [ -n "$API_ADVERTISE_ADDRESSES" ]
-then
-    # shellcheck disable=SC2154
-    kubeadm init --token "${kubeadm_token}" --kubernetes-version=v1.6.4 --api-advertise-address="$API_ADVERTISE_ADDRESSES"
-else
-    # shellcheck disable=SC2154
-    kubeadm init --token "${kubeadm_token}" --kubernetes-version=v1.6.4
-fi
+       echo "Inititializing the master...."
 
-# Copy kubernetes configuration created by kubeadm (admin.conf to .kube/config)
-# shellcheck disable=SC2154
-SSH_USER="${ssh_user}"
-mkdir -p "/home/$SSH_USER/.kube/"
-chown "$SSH_USER":"$SSH_USER" "/home/$SSH_USER/.kube/"
-cp "/etc/kubernetes/admin.conf" "/home/$SSH_USER/.kube/config"
-chown "$SSH_USER":"$SSH_USER" "/home/$SSH_USER/.kube/config"
+       if [ -n "$API_ADVERTISE_ADDRESSES" ]
+       then
+           # shellcheck disable=SC2154
+           kubeadm init --token "${kubeadm_token}" --kubernetes-version=v1.6.4 --api-advertise-address="$API_ADVERTISE_ADDRESSES"
+       else
+           # shellcheck disable=SC2154
+           kubeadm init --token "${kubeadm_token}" --kubernetes-version=v1.6.4
+       fi
+
+       # Copy kubernetes configuration created by kubeadm (admin.conf to .kube/config)
+       # shellcheck disable=SC2154
+       SSH_USER="${ssh_user}"
+       mkdir -p "/home/$SSH_USER/.kube/"
+       chown "$SSH_USER":"$SSH_USER" "/home/$SSH_USER/.kube/"
+       cp "/etc/kubernetes/admin.conf" "/home/$SSH_USER/.kube/config"
+       chown "$SSH_USER":"$SSH_USER" "/home/$SSH_USER/.kube/config"
 
--- bootstrap/node.sh
+++ bootstrap/node.sh
@@ -1,34 +1,37 @@
-#!/bin/bash
+       #!/bin/bash
 
-# Add hostname
-echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+       # OBS This file has to be indented 6 spaces to fit into cloud config yaml
+       # as inline bash file
 
-# Taint and label
-node_labels=${node_labels}
-node_taints=${node_taints}
+       # Add hostname
+       echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 
-echo "Label nodes"
-if [ -n "$node_labels" ]
-then
-    sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=$node_labels |g" \
-       /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-fi
+       # Taint and label
+       node_labels=${node_labels}
+       node_taints=${node_taints}
 
-echo "Taint nodes"
-if [ -n "$node_taints" ]
-then
-    sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--register-with-taints=$node_taints |g" \
-       /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-fi
+       echo "Label nodes"
+       if [ -n "$node_labels" ]
+       then
+           sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=$node_labels |g" \
+              /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+       fi
 
-# reload and restart after systemd dropin edits
-systemctl daemon-reload
-systemctl restart kubelet
+       echo "Taint nodes"
+       if [ -n "$node_taints" ]
+       then
+           sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--register-with-taints=$node_taints |g" \
+              /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+       fi
 
-# execute modprobe on node - workaround for heketi gluster
-echo "Modprobe dm_thin_pool"
-modprobe dm_thin_pool
+       # reload and restart after systemd dropin edits
+       systemctl daemon-reload
+       systemctl restart kubelet
 
-echo "Try to join master..."
-# shellcheck disable=SC2154
-kubeadm join --token "${kubeadm_token}" "${master_ip}:6443"
+       # execute modprobe on node - workaround for heketi gluster
+       echo "Modprobe dm_thin_pool"
+       modprobe dm_thin_pool
+
+       echo "Try to join master..."
+       # shellcheck disable=SC2154
+       kubeadm join --token "${kubeadm_token}" "${master_ip}:6443"
