bd62fd21d6b5d19937fdc1c5c5991ccfdee1d15e
Anders Larsson <anders.larsson@icm.uu.se>
2017-10-02 19:35:17 +0200

    update to new image that includes upgrade to kubernetes 1.7.5

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -34,7 +34,7 @@ echo "Inititializing the master...."
 if [ -n "$API_ADVERTISE_ADDRESSES" ]
 then
     # shellcheck disable=SC2154
-    kubeadm init --token "${kubeadm_token}" --kubernetes-version=v1.6.4 --api-advertise-address="$API_ADVERTISE_ADDRESSES"
+    kubeadm init --token "${kubeadm_token}" --kubernetes-version=v1.7.5 --api-advertise-address="$API_ADVERTISE_ADDRESSES"
 else
     # shellcheck disable=SC2154
     kubeadm init --token "${kubeadm_token}" --kubernetes-version=v1.6.4
