ec65101fcc1835914bb45dbb69244514c6a692a8
Anders Larsson <anders.larsson@icm.uu.se>
2017-11-21 19:41:57 +0100

    updates

--- common/inventory/main.tf
+++ common/inventory/main.tf
@@ -5,101 +5,101 @@ variable domain {}
 variable master_as_edge {}
 
 variable master_hostnames {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable master_public_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable master_private_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable master_labels {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable node_count {}
 
 variable node_hostnames {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable node_labels {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable node_public_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable node_private_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable infra_hostnames {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable infra_labels {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable infra_public_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable infra_private_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable bastion_hostnames {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable bastion_public_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable bastion_private_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable bastion_labels {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable edge_count {}
 
 variable edge_hostnames {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable edge_public_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
 variable edge_private_ip {
-  type = "list"
+  type    = "list"
   default = [""]
 }
 
@@ -115,29 +115,28 @@ variable inventory_output_file {
 
 # create variables
 locals {
-
   master_hostnames = "${split(",", length(var.master_hostnames) == 0 ? join(",", list("")) : join(",", var.master_hostnames))}"
   master_public_ip = "${split(",", length(var.master_public_ip) == 0 ? join(",", list("")) : join(",", var.master_public_ip))}"
   master_labels    = "${split(",", length(var.master_labels) == 0 ? join(",", list("")) : join(",", var.master_labels))}"
-  
+
   node_hostnames = "${split(",", length(var.node_hostnames) == 0 ? join(",", list("")) : join(",", var.node_hostnames))}"
   node_public_ip = "${split(",", length(var.node_public_ip) == 0 ? join(",", list("")) : join(",", var.node_public_ip))}"
   node_labels    = "${split(",", length(var.node_labels) == 0 ? join(",", list("")) : join(",", var.node_labels))}"
-  
+
   bastion_hostnames = "${split(",", length(var.bastion_hostnames) == 0 ? join(",", list("")) : join(",", var.bastion_hostnames))}"
   bastion_public_ip = "${split(",", length(var.bastion_public_ip) == 0 ? join(",", list("")) : join(",", var.bastion_public_ip))}"
   bastion_labels    = "${split(",", length(var.bastion_labels) == 0 ? join(",", list("")) : join(",", var.bastion_labels))}"
-  
+
   infra_hostnames = "${split(",", length(var.infra_hostnames) == 0 ? join(",", list("")) : join(",", var.infra_hostnames))}"
   infra_public_ip = "${split(",", length(var.infra_public_ip) == 0 ? join(",", list("")) : join(",", var.infra_public_ip))}"
   infra_labels    = "${split(",", length(var.infra_labels) == 0 ? join(",", list("")) : join(",", var.infra_labels))}"
 
   # Format list of different node types
-  masters  = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s openshift_public_ip=%s openshift_node_labels=%s", local.master_hostnames , local.master_public_ip, var.ssh_user, local.master_public_ip, local.master_labels ))}"
+  masters = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s openshift_public_ip=%s openshift_node_labels=%s", local.master_hostnames , local.master_public_ip, var.ssh_user, local.master_public_ip, local.master_labels ))}"
 
-  nodes    = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s openshift_public_ip=%s openshift_node_labels=%s", local.node_hostnames , local.node_public_ip, var.ssh_user, local.node_public_ip, local.node_labels ))}"
-  
-  infra    = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s openshift_public_ip=%s openshift_node_labels=%s", local.infra_hostnames , local.infra_public_ip, var.ssh_user, local.infra_public_ip, local.infra_labels ))}"
+  nodes = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s openshift_public_ip=%s openshift_node_labels=%s", local.node_hostnames , local.node_public_ip, var.ssh_user, local.node_public_ip, local.node_labels ))}"
+
+  infra = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s openshift_public_ip=%s openshift_node_labels=%s", local.infra_hostnames , local.infra_public_ip, var.ssh_user, local.infra_public_ip, local.infra_labels ))}"
 
   bastions = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s openshift_public_ip=%s openshift_node_labels=%s", local.bastion_hostnames , local.bastion_public_ip, var.ssh_user, local.bastion_public_ip, local.bastion_labels ))}"
 
