dde3cef4cf72b2d2cf1396eb21887fc3d4910ee3
Anders Larsson <anders.larsson@icm.uu.se>
2017-12-13 09:25:11 +0100

    updated comments and lowercased some vars [skip ci]

--- bin/image-create-azure.sh
+++ bin/image-create-azure.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+#
+# Via azure command line client check if specified Image is present and
+# upload it to the users Azure account. The image will be uploaded to a
+# generated resource-group in a generated storage-account in a speciffic
+# location.
+#
+# Env vars
+#   IMAGE_NAME
+#   TF_VARS_FILE
 
 # Exit immediately if a command exits with a non-zero status
 set -e
@@ -19,41 +28,37 @@ if [ -z "${TF_VARS_FILE}" ]; then
 fi
 
 # Get vars from tfvars-file
-if [ -z "${ARM_CLIENT_ID}" ]; then
-  ARM_CLIENT_ID=$(grep "client_id" "$TF_VARS_FILE" |
+arm_client_id=$(grep "client_id" "$TF_VARS_FILE" |
     cut -d "=" -f 2- |
     awk -F\" '{print $(NF-1)}')
-fi
-if [ -z "${ARM_CLIENT_SECRET}" ]; then
-  ARM_CLIENT_SECRET=$(grep "client_secret" "$TF_VARS_FILE" |
+
+arm_client_secret=$(grep "client_secret" "$TF_VARS_FILE" |
     cut -d "=" -f 2- |
     awk -F\" '{print $(NF-1)}')
-fi
-if [ -z "${ARM_TENANT_ID}" ]; then
-  ARM_TENANT_ID=$(grep "tenant_id" "$TF_VARS_FILE" |
+
+arm_tenant_id=$(grep "tenant_id" "$TF_VARS_FILE" |
     cut -d "=" -f 2- |
     awk -F\" '{print $(NF-1)}')
-fi
-if [ -z "${ARM_LOCATION}" ]; then
-  ARM_LOCATION=$(grep "location" "$TF_VARS_FILE" |
+
+arm_location=$(grep "location" "$TF_VARS_FILE" |
     cut -d "=" -f 2- |
     awk -F\" '{print $(NF-1)}')
-fi
 
 echo "Login"
 az login --service-principal \
-  -u "$ARM_CLIENT_ID" \
-  -p "$ARM_CLIENT_SECRET" \
-  --tenant "$ARM_TENANT_ID" \
+  -u "$arm_client_id" \
+  -p "$arm_client_secret" \
+  --tenant "$arm_tenant_id" \
   --output "$OUTPUT_FMT"
 
-# Make sure location is in lowercase format without spaces
-location_short="${ARM_LOCATION//[[:blank:]]/}"
+# Make sure location is in azure short lowercase format without spaces
+location_short="${arm_location//[[:blank:]]/}"
 location_short="${location_short,,}"
 
-# append location to rg to make unique rg per location
+# Append location to rg to make unique rg per location
 resource_group="$RESOURCE_GROUP_PREFIX-$location_short"
 
+# Check if image exists in this resource-group in this location
 image_details=$(az image show --resource-group "$resource_group" --name "$IMAGE_NAME" -o json |
   jq "select(.location == \"$location_short\")")
 if [ -z "$image_details" ]; then
@@ -66,7 +71,9 @@ if [ -z "$image_details" ]; then
     --output "$OUTPUT_FMT"
 
   echo "Create storage account (if not there already)"
-  # create a uniqe (>1 in a quadrillion), and stable suffix via md5sum of subscription-id + location
+  # Azure storage account names need to be unique.
+  # Create a uniqe (>1 in a quadrillion), and stable suffix via
+  # md5sum of subscription-id + location
   subscription_id=$(az account show --query id | tr -d '"')
   suffix=$(md5sum <<<"$subscription_id$location_short" | head -c 14)
   storage_account="kubenow000$suffix"
@@ -108,13 +115,15 @@ if [ -z "$image_details" ]; then
     --output "$OUTPUT_FMT" &&
     true
 
-  # Check file copy progress by polling the show blob status
-  # The loop also updates the copy progress message to user and
+  # Check file copy progress by polling the show blob status.
+  # This loop also updates the copy progress message to user and
   # updates the spinner character
   spin='-\|/'
   for i in {0..36000}; do
     n=$((i % 4))
     m=$((i % 6))
+
+    # Every m time poll copy progress
     if [ $m == 0 ]; then
       progress=$(az storage blob show --name "$file_name_vhd" \
         --container-name kubenow-images \
@@ -133,6 +142,7 @@ if [ -z "$image_details" ]; then
       percent=$(bc -l <<<"if ($percent > 100) 99.99 else $percent")
     fi
 
+    # Every time print status message with an updated spinner symbol
     printf '\r %s Image copy progress: %.2f%%' "${spin:$n:1}" "$percent"
 
     # Break when finished
