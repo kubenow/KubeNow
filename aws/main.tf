05e26d4fbdc97f7521bcfd3017e07ed789baf326
Anders Larsson <anders.larsson@icm.uu.se>
2018-03-17 16:59:35 +0100

    add forgotten aws config

--- aws/main.tf
+++ aws/main.tf
@@ -199,9 +199,9 @@ module "master" {
   extra_disk_size = "0"
 
   # Bootstrap settings
-  bootstrap_file = "bootstrap/master.sh"
+  bootstrap_file = "${var.bootstrap_script}"
   kubeadm_token  = "${var.kubeadm_token}"
-  node_labels    = "${split(",", var.master_as_edge == "true" ? "role=edge" : "")}"
+  node_labels    = "${split(",", var.master_as_edge == "true" ? "role=master,role=edge" : "role=master")}"
   node_taints    = [""]
   master_ip      = ""
 }
@@ -228,7 +228,7 @@ module "node" {
   extra_disk_size = "0"
 
   # Bootstrap settings
-  bootstrap_file = "bootstrap/node.sh"
+  bootstrap_file = "${var.bootstrap_script}"
   kubeadm_token  = "${var.kubeadm_token}"
   node_labels    = ["role=node"]
   node_taints    = [""]
@@ -257,7 +257,7 @@ module "edge" {
   extra_disk_size = "0"
 
   # Bootstrap settings
-  bootstrap_file = "bootstrap/node.sh"
+  bootstrap_file = "${var.bootstrap_script}"
   kubeadm_token  = "${var.kubeadm_token}"
   node_labels    = ["role=edge"]
   node_taints    = [""]
@@ -286,7 +286,7 @@ module "glusternode" {
   extra_disk_size = "${var.glusternode_extra_disk_size}"
 
   # Bootstrap settings
-  bootstrap_file = "bootstrap/node.sh"
+  bootstrap_file = "${var.bootstrap_script}"
   kubeadm_token  = "${var.kubeadm_token}"
   node_labels    = ["storagenode=glusterfs"]
   node_taints    = [""]
@@ -316,23 +316,24 @@ module "cloudflare" {
 
 # Generate Ansible inventory (identical for each cloud provider)
 module "generate-inventory" {
-  source             = "../common/inventory"
-  cluster_prefix     = "${var.cluster_prefix}"
-  domain             = "${ var.use_cloudflare == true ? module.cloudflare.domain_and_subdomain : format("%s.nip.io", element(concat(module.edge.public_ip, module.master.public_ip), 0))}"
-  ssh_user           = "${var.ssh_user}"
-  master_hostnames   = "${module.master.hostnames}"
-  master_public_ip   = "${module.master.public_ip}"
-  master_private_ip  = "${module.master.local_ip_v4}"
-  master_as_edge     = "${var.master_as_edge}"
-  edge_count         = "${var.edge_count}"
-  edge_hostnames     = "${module.edge.hostnames}"
-  edge_public_ip     = "${module.edge.public_ip}"
-  edge_private_ip    = "${module.edge.local_ip_v4}"
-  node_count         = "${var.node_count}"
-  node_hostnames     = "${module.node.hostnames}"
-  node_public_ip     = "${module.node.public_ip}"
-  node_private_ip    = "${module.node.local_ip_v4}"
-  glusternode_count  = "${var.glusternode_count}"
-  gluster_volumetype = "${var.gluster_volumetype}"
-  extra_disk_device  = "${element(concat(module.glusternode.extra_disk_device, list("")),0)}"
+  source                 = "../common/inventory"
+  cluster_prefix         = "${var.cluster_prefix}"
+  domain                 = "${var.use_cloudflare == true ? module.cloudflare.domain_and_subdomain : format("%s.nip.io", element(concat(module.edge.public_ip, module.master.public_ip, list("")), 0))}"
+  ssh_user               = "${var.ssh_user}"
+  master_hostnames       = "${module.master.hostnames}"
+  master_public_ip       = "${module.master.public_ip}"
+  master_private_ip      = "${module.master.local_ip_v4}"
+  master_as_edge         = "${var.master_as_edge}"
+  edge_count             = "${var.edge_count}"
+  edge_hostnames         = "${module.edge.hostnames}"
+  edge_public_ip         = "${module.edge.public_ip}"
+  edge_private_ip        = "${module.edge.local_ip_v4}"
+  node_count             = "${var.node_count}"
+  node_hostnames         = "${module.node.hostnames}"
+  node_public_ip         = "${module.node.public_ip}"
+  node_private_ip        = "${module.node.local_ip_v4}"
+  glusternode_count      = "${var.glusternode_count}"
+  gluster_volumetype     = "${var.gluster_volumetype}"
+  gluster_extra_disk_dev = "${element(concat(module.glusternode.extra_disk_device, list("")),0)}"
+  inventory_template     = "${var.inventory_template}"
 }
