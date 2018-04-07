155c9f213e1495296076fd4b65f38ce92fd6fb92
Stephen Moloney <stephen@stephenmoloney.com>
2018-04-07 19:03:10 +0100

    partial commit

--- bin/image-create-openstack.sh
+++ bin/image-create-openstack.sh
@@ -12,6 +12,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+source ./openstack/functions.sh
+
 echo "Started script image-create-openstack"
 
 if [ -z "$KN_IMAGE_NAME" ]; then
@@ -24,92 +26,19 @@ if [ -z "${KN_DISC_FORMAT}" ]; then
   exit 1
 fi
 
+
 KN_IMAGE_BUCKET_URL=${KN_IMAGE_BUCKET_URL:-"https://s3.eu-central-1.amazonaws.com/kubenow-eu-central-1"}
-file_name="$KN_IMAGE_NAME.qcow2"
+download_file_name="${KN_IMAGE_NAME}.qcow2"
+upload_file_name="${KN_IMAGE_NAME}.${KN_DISC_FORMAT}"
 
-# Check if image is present already
-echo "List images available in OpenStack..."
-image_list="$(glance image-list)"
-image_id="$(printf '%s' "$image_list" |
-  grep "\s$KN_IMAGE_NAME\s" |
-  awk -F "|" '{print $2;}' |
-  tr -d '[:space:]')"
+# 1. Possibly download the image
+maybe_download_image ${KN_IMAGE_NAME} ${KN_DISC_FORMAT} ${KN_IMAGE_BUCKET_URL}
 
-# If it exist then exit here
-if [ -n "$image_id" ]; then
-  echo "file exists - no need to upload, exit image-upload script"
-  exit 0
-fi
+# 2. Convert image
 
-# If it doesn't exist then download it
-echo "Image not present in OpenStack"
-echo "Downloading image to local /tmp/"
-curl "$KN_IMAGE_BUCKET_URL/$file_name" \
-  -o "/tmp/$file_name" \
-  --connect-timeout 30 \
-  --max-time 1800
 
-echo "Download md5 sum file"
-curl "$KN_IMAGE_BUCKET_URL/$file_name.md5" \
-  -o "/tmp/$file_name.md5" \
-  --connect-timeout 30 \
-  --max-time 1800
+# 3. Upload image
 
-# Verify md5sum of downloaded file
-echo "Check md5 sum"
-md5result=$(
-  cd /tmp
-  md5sum -c "$file_name.md5"
-)
-if [[ "$md5result" != *": OK"* ]]; then
-  echo >&2 "Wrong checksum of downloaded image."
-  echo >&2 "Something might have failed on file transfer."
-  echo >&2 "Please try again."
-  exit 1
-else
-  echo "Checksum of downloaded image OK"
-fi
 
-# Upload image
-echo "Uploading image"
-glance image-create \
-  --file "/tmp/$file_name" \
-  --disk-format ${KN_DISC_FORMAT} \
-  --min-disk 20 \
-  --container-format bare \
-  --name "$KN_IMAGE_NAME" \
-  --progress
-
-echo "Verify md5 of present/uploaded image..."
-echo "List images available in OpenStack..."
-image_list="$(glance image-list)"
-image_id="$(printf '%s' "$image_list" |
-  grep "\s$KN_IMAGE_NAME\s" |
-  awk -F "|" '{print $2;}' |
-  tr -d '[:space:]')"
-
-# Get checksum of uploaded file
-image_details="$(glance image-show "$image_id")"
-checksum="$(printf '%s' "$image_details" |
-  grep -w "checksum" |
-  awk -F "|" '{print $3;}' |
-  tr -d '[:space:]')"
-
-# Get checksum of bucket image
-echo "Download md5 sum file"
-curl "$KN_IMAGE_BUCKET_URL/$file_name.md5" \
-  -o "/tmp/$file_name.md5" \
-  --connect-timeout 30 \
-  --max-time 1800
-
-md5only=$(cut -f1 -d ' ' "/tmp/$file_name.md5")
-if [ "$md5only" != "$checksum" ]; then
-  echo >&2 "Wrong checksum of present/uploaded image."
-  echo >&2 "Something might have failed on file transfer."
-  echo >&2 "Please delete image $KN_IMAGE_NAME from Openstack and try again."
-  exit 1
-else
-  echo "Checksum OK"
-fi
 
 echo "Image upload done"
