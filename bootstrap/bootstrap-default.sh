5549abcf1f42fdf3641852c33b69300c06889a52
Anders Larsson <anders.larsson@icm.uu.se>
2018-07-13 14:16:22 +0200

    openstack cloud integration [skip ci]

--- bootstrap/bootstrap-default.sh
+++ bootstrap/bootstrap-default.sh
@@ -24,6 +24,19 @@ if [ -n "$node_taints" ]; then
     /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 fi
 
+echo "Add cloud-config file"
+cat > /etc/kubernetes/cloud-config <<EOL
+[Global]
+auth-url=" - some auth url param e.g. https://uppmax.cloud.snic.se:5000/v3"
+tenant-id=" - same as project id - e.g. 9301f656901b45c291887b5012f44a20" 
+username=" - your user id, e.g. s6215" 
+password=" - user password"  
+EOL
+
+echo "Add cloud-config flag"
+sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--cloud-provider=openstack --cloud-provider=openstack --cloud-config=/etc/kubernetes/cloud-config |g" \
+  /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
 # reload and restart after systemd dropin edits
 systemctl daemon-reload
 systemctl restart kubelet
@@ -41,13 +54,33 @@ sudo sed -i '/swap/d' /etc/fstab
 if [[ "$node_labels" == *"role=master"* ]]; then
   echo "Inititializing the master...."
 
+  echo "Create kubeadm conf-file"
+  echo "kind: MasterConfiguration" > /etc/kubernetes/kubeadm-extra.config
+  echo "apiVersion: kubeadm.k8s.io/v1alpha1" >> /etc/kubernetes/kubeadm-extra.config
+  echo "kubernetesVersion: v1.9.2" >> /etc/kubernetes/kubeadm-extra.config
+  # I did not get this bootstrap tokens in this config to work Instead create below
+  echo "bootstrapTokens:" >> /etc/kubernetes/kubeadm-extra.config
+  echo "- groups:" >> /etc/kubernetes/kubeadm-extra.config
+  echo "  - system:bootstrappers:kubeadm:default-node-token" >> /etc/kubernetes/kubeadm-extra.config
+  echo "  token: \"${kubeadm_token}\"" >> /etc/kubernetes/kubeadm-extra.config
+  echo "  ttl: 0" >> /etc/kubernetes/kubeadm-extra.config
+  echo "  usages:" >> /etc/kubernetes/kubeadm-extra.config
+  echo "  - signing" >> /etc/kubernetes/kubeadm-extra.config
+  echo "  - authentication" >> /etc/kubernetes/kubeadm-extra.config
+  echo "networking:" >> /etc/kubernetes/kubeadm-extra.config
+  echo "  podSubnet: 10.244.0.0/16" >> /etc/kubernetes/kubeadm-extra.config
+  echo "cloudProvider: openstack" >> /etc/kubernetes/kubeadm-extra.config
+  
   if [ -n "$API_ADVERTISE_ADDRESSES" ]; then
-    # shellcheck disable=SC2154
-    kubeadm init --token "${kubeadm_token}" --token-ttl=0 --pod-network-cidr=10.244.0.0/16 --kubernetes-version=v1.9.2 --api-advertise-address="$API_ADVERTISE_ADDRESSES"
-  else
-    # shellcheck disable=SC2154
-    kubeadm init --token "${kubeadm_token}" --token-ttl=0 --pod-network-cidr=10.244.0.0/16 --kubernetes-version=v1.9.2
+    echo "api:" >> /etc/kubernetes/kubeadm-extra.config
+    echo "  advertiseAddress: $API_ADVERTISE_ADDRESSES" >> /etc/kubernetes/kubeadm-extra.config
   fi
+  
+  # Init cluster (master)
+  kubeadm init --config "/etc/kubernetes/kubeadm-extra.config"
+  # create token (has to be done after init - otherwise kubeadm config files are missing)
+  # shellcheck disable=SC2154
+  kubeadm token create --ttl 0 "${kubeadm_token}"
 
   # Copy Kubernetes configuration created by kubeadm (admin.conf to .kube/config)
   # shellcheck disable=SC2154
