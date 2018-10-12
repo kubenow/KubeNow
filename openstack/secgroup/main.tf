2912c98978838a64b09b27fdc923db55cc6b1944
Anders Larsson <anders.larsson@icm.uu.se>
2018-10-12 14:55:34 +0200

    first version of secgroup script

--- openstack/secgroup/main.tf
+++ openstack/secgroup/main.tf
@@ -1,46 +1,54 @@
 variable name_prefix {}
 variable secgroup_name {}
 
-resource "openstack_compute_secgroup_v2" "created" {
+variable ingress_tcp_ports {
+  default = ["22", "80", "443", "480"]
+}
+
+resource "openstack_networking_secgroup_v2" "created" {
   # create only if not specified in var.secgroup_name
   count       = "${var.secgroup_name == "" ? 1 : 0}"
   name        = "${var.name_prefix}-secgroup"
   description = "The automatically created secgroup for ${var.name_prefix}"
+}
 
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_allow_all_internal" {
+  # create only if not specified in var.secgroup_name
+  count = "${var.secgroup_name == "" ? 1 : 0}"
 
-  rule {
-    from_port   = 80
-    to_port     = 80
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = "1"
+  port_range_max    = "64535"
+  remote_group_id   = "${openstack_networking_secgroup_v2.created.id}"
+  security_group_id = "${openstack_networking_secgroup_v2.created.id}"
+}
 
-  rule {
-    from_port   = 443
-    to_port     = 443
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_allow_all_internal2" {
+  # create only if not specified in var.secgroup_name
+  count = "${var.secgroup_name == "" ? 1 : 0}"
 
-  rule {
-    from_port   = 1      # All internal tcp traffic
-    to_port     = 65535
-    ip_protocol = "tcp"
-    self        = "true"
-  }
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = "1"
+  port_range_max    = "64535"
+  remote_group_id   = "${openstack_networking_secgroup_v2.created.id}"
+  security_group_id = "${openstack_networking_secgroup_v2.created.id}"
+}
 
-  rule {
-    from_port   = 1      # All internal udp traffic
-    to_port     = 65535
-    ip_protocol = "udp"
-    self        = "true"
-  }
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ingress_tcp_ports" {
+  # create only if not specified in var.secgroup_name
+  count = "${var.secgroup_name == "" ? length(var.ingress_tcp_ports) : 0}"
+
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = "${element(var.ingress_tcp_ports, count.index)}"
+  port_range_max    = "${element(var.ingress_tcp_ports, count.index)}"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.created.id}"
 }
 
 output "secgroup_name" {
@@ -51,5 +59,5 @@ output "secgroup_name" {
   #
   # This is tracked upstream: https://github.com/hashicorp/hil/issues/50
   #
-  value = "${ var.secgroup_name == "" ? join(" ", openstack_compute_secgroup_v2.created.*.name) : var.secgroup_name }"
+  value = "${ var.secgroup_name == "" ? join(" ", openstack_networking_secgroup_v2.created.*.name) : var.secgroup_name }"
 }
