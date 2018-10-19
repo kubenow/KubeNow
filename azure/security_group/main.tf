19d2de8cae5afff073eb06a949fba2958b69a74c
Anders Larsson <anders.larsson@icm.uu.se>
2018-10-19 08:49:30 +0200

    terraform format

--- azure/security_group/main.tf
+++ azure/security_group/main.tf
@@ -15,16 +15,16 @@ resource "azurerm_network_security_group" "main" {
 resource "azurerm_network_security_rule" "security_rule_ingress_tcp_port" {
   count = "${length(var.ingress_tcp_ports)}"
 
-  name                        = "${var.name_prefix}-secrule-${format("%03d", count.index)}"
-  description                 = "Automatically created security rule by-${var.name_prefix}"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "${element(var.ingress_tcp_ports, count.index)}"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  name                       = "${var.name_prefix}-secrule-${format("%03d", count.index)}"
+  description                = "Automatically created security rule by-${var.name_prefix}"
+  priority                   = 100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "${element(var.ingress_tcp_ports, count.index)}"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
 
   resource_group_name         = "${var.resource_group_name}"
   network_security_group_name = "${azurerm_network_security_group.main.name}"
