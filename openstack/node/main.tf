c9a4ee43f8921ed1ba63343188812e962482165a
Marco Enrico Piras <kikkomep@crs4.it>
2018-11-28 15:04:23 +0100

    Set the public ip when available

--- openstack/node/main.tf
+++ openstack/node/main.tf
@@ -118,8 +118,8 @@ output "local_ip_v4" {
   value = ["${openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4}"]
 }
 
-output "public_ip" {
-  value = ["${openstack_compute_instance_v2.instance.*.network.1.fixed_ip_v4}"]  
+output "public_ip" {    
+  value = ["${openstack_compute_floatingip_v2.floating_ip.*.address}", "${openstack_compute_instance_v2.instance.*.network.1.fixed_ip_v4}"]
 }
 
 output "hostnames" {
