78f75e193ee5dc269e575d772ebe1ff99bfedfc3
andersla <anders@ormbunkar.se>
2018-12-06 08:28:17 +0100

    add rancher bootstrap file [skip ci]

---
+++ bootstrap/bootstrap-default-rancer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# Add hostname
+echo "127.0.0.1 $HOSTNAME" >>/etc/hosts
+
+echo "Ensure that APT works with HTTPS..."
+sudo apt-get -qq update -y
+sudo apt-get -qq install -y \
+  apt-transport-https \
+  ca-certificates \
+  software-properties-common \
+  curl
+
+echo "Add Docker repo..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "Updating Ubuntu..."
+sudo apt-get -qq update -y
+sudo DEBIAN_FRONTEND=noninteractive \
+  apt-get -y -qq \
+  -o Dpkg::Options::="--force-confdef" \
+  -o Dpkg::Options::="--force-confold" \
+  upgrade
+
+echo "Installing Kubernetes requirements..."
+sudo apt-get -qq install -y \
+  docker-ce=17.03.3~ce-0~ubuntu-xenial
+
+sudo usermod -aG docker ubuntu
+
+echo "Bootstrap finished OK"
