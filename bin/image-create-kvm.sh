6d8641280212bb39dd20302c009e7867bf2ec048
andersla <anders@ormbunkar.se>
2019-01-10 13:56:39 +0100

    update kvm-download-script

--- bin/image-create-kvm.sh
+++ bin/image-create-kvm.sh
@@ -10,29 +10,38 @@
 set -e
 
 function is_checksum_ok() {
+  local checksum="$1"
+  local source_file="$2"
+  
   echo "Verify checksum"
-  # local source_file="$1"
-  echo 0
-}
-
-function download_checksum_file() {
-  echo "Download checksum"
-  # local source_file="$1"
-  echo 0
+  
+  sha256result=$(
+    sha256sum -c <<< "$checksum $source_file"
+  )
+  
+  if [[ "$sha256result" != *": OK"* ]]; then
+    return 1
+  else
+    return 0
+  fi
 }
 
 function download_image() {
   local file_url="$1"
   local dest_dir="$2"
   local file_name="$3"
-
-  echo "Downloading image $file_name to $dest_dir"
-
-  mkdir -p "$dest_dir"
-  curl "$file_url" \
-    -o "$dest_dir/$file_name" \
-    --connect-timeout 30 \
-    --max-time 1800
+  
+  if [ ! -f "$dest_dir/$file_name" ]; then
+    echo "Downloading image $file_name to $dest_dir"
+    mkdir -p "$dest_dir"
+    curl "$file_url" \
+      -o "$dest_dir/$file_name" \
+      --fail \
+      --connect-timeout 30 \
+      --max-time 1800
+  fi
+  
+  return 0
 }
 
 echo "Started script image-create-kvm"
@@ -46,49 +55,57 @@ KN_IMAGE_BUCKET_URL=${KN_IMAGE_BUCKET_URL:-"https://s3.eu-central-1.amazonaws.co
 file_name="$KN_IMAGE_NAME"
 local_dir=${KN_LOCAL_DIR:="kvm-image"}
 temp_dir="/tmp/kvm-image"
+checksum="$KN_IMAGE_SHA256_SUM"
 
 # Add .qcow suffix if it is a KubeNow image
 if [[ "$file_name" == kubenow* ]]; then
   file_name="$file_name.qcow"
 fi
 
-# Check if file exist
-if [ -f "$temp_dir/$file_name" ]; then
-  # Check if checksum ok
-  if is_checksum_ok; then
-    echo "File exists, checksum is OK."
-
-    # move image to final dest if not there
-    if [ ! -f "$local_dir/$file_name" ]; then
-      echo "Copy file to $local_dir/$file_name"
-      mkdir -p "$local_dir"
-      cp "$temp_dir/$file_name" "$local_dir/$file_name"
-      exit 0
+# check if file is in final dir
+if [ -f "$local_dir/$file_name" ]; then
+  echo "File exists in final dir"
+  
+  # Checksum
+  if [ -n "$KN_IMAGE_SHA256_SUM" ]; then
+    if is_checksum_ok "$checksum" "$local_dir/$file_name"; then
+      echo "Checksum is ok"
     else
-      echo "File exists in final dir"
-      exit 0
+      echo "Checksum of file: $local_dir/$file_name is wrong, exiting"
+      exit 1
     fi
-  else
-    echo "File exists, checksum is wrong, continuing..."
   fi
-else
-  echo "File does not exist localy"
+  
+  exit 0
 fi
 
-# Download image
-download_image "$KN_IMAGE_BUCKET_URL/$file_name" "$temp_dir" "$file_name"
-
+# Download if needed
 if [ ! -f "$temp_dir/$file_name" ]; then
-  echo "Could not download file ok"
-  exit 1
+  # Download image
+  download_image "$KN_IMAGE_BUCKET_URL/$file_name" "$temp_dir" "$file_name" "$checksum"
 fi
 
-if ! is_checksum_ok; then
-  echo "Checksum of downloaded file is not correct"
-  exit 1
+# Checksum
+if [ -n "$KN_IMAGE_SHA256_SUM" ]; then
+  if is_checksum_ok "$checksum" "$temp_dir/$file_name"; then
+    echo "Checksum is ok"
+  else
+    echo "Checksum of downloaded file: $temp_dir/$file_name is wrong, exiting"
+    exit 1
+  fi
 fi
 
 # Move image to final dest
 echo "Copy file to $local_dir/$file_name"
 mkdir -p "$local_dir"
 cp "$temp_dir/$file_name" "$local_dir/$file_name"
+
+# Checksum
+if [ -n "$KN_IMAGE_SHA256_SUM" ]; then
+  if is_checksum_ok "$checksum" "$local_dir/$file_name"; then
+    echo "Checksum is ok"
+  else
+    echo "Checksum of file: $local_dir/$file_name is wrong, exiting"
+    exit 1
+  fi
+fi
