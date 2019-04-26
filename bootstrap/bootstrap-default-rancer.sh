0132fee5ad6e76c8b741ec26121a792db09ee7b4
Anders Larsson <anders@ormbunkar.se>
2019-04-26 07:44:32 +0200

    update racher-bootstrap

--- bootstrap/bootstrap-default-rancer.sh
+++ bootstrap/bootstrap-default-rancer.sh
@@ -5,30 +5,13 @@ set -e
 # Add hostname
 echo "127.0.0.1 $HOSTNAME" >>/etc/hosts
 
-#echo "Ensure that APT works with HTTPS..."
-#sudo apt-get -qq update -y
-#sudo apt-get -qq install -y \
-#  apt-transport-https \
-#  ca-certificates \
-#  software-properties-common \
-#  curl
-#echo "Add Docker repo..."
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-#echo "Updating Ubuntu..."
-#sudo apt-get -qq update -y
-#sudo DEBIAN_FRONTEND=noninteractive \
-#  apt-get -y -qq \
-#  -o Dpkg::Options::="--force-confdef" \
-#  -o Dpkg::Options::="--force-confold" \
-#  upgrade
-
-#echo "Installing Kubernetes requirements..."
-#sudo apt-get -qq install -y \
-#  docker-ce=17.03.3~ce-0~ubuntu-xenial
-
+# Rancher prerequisites
+# see: https://rancher.com/docs/rke/latest/en/os/
 curl "releases.rancher.com/install-docker/17.03.sh" | bash
+# User need to be in docker group
 sudo usermod -aG docker ubuntu
 
-echo "Bootstrap finished OK"
+# Extra requirements
+sudo apt-get -qq install -y
+
+echo "Bootstrap finished"
