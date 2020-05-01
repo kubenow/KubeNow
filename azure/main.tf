5ed9f5c2bf3ffe2cdd4f47151c8c2c3c44f26a60
Steffen Neumann <sneumann@ipb-halle.de>
2020-05-01 20:27:51 +0200

    change to new cloudflare 2.0 API with api_token and zone_id

--- azure/main.tf
+++ azure/main.tf
@@ -90,6 +90,10 @@ variable cloudflare_subdomain {
   default = ""
 }
 
+variable cloudflare_zone_id {
+  default = ""
+}
+
 variable cloudflare_proxied {
   default = "false"
 }
@@ -261,6 +265,7 @@ module "cloudflare" {
   cloudflare_token     = "${var.cloudflare_token}"
   cloudflare_domain    = "${var.cloudflare_domain}"
   cloudflare_subdomain = "${var.cloudflare_subdomain}"
+  cloudflare_zone_id   = "${var.cloudflare_zone_id}"
 
   # add optional subdomain to record names
   # terraform interpolation is limited and can not return list in conditionals, workaround: first join to string, then split
